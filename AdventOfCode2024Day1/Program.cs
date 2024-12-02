e924713f2b18ffa250cbcc090429ea6b8383ff22
David Langr <git@langr.tech>
2024-12-02 14:15:16 +0100

    Restructure the data to make sorting easier

--- AdventOfCode2024Day1/Program.cs
+++ AdventOfCode2024Day1/Program.cs
@@ -4,21 +4,25 @@
 var file = File.ReadLines("Day1.txt");
 var fileArray = file as string[] ?? file.ToArray();
 
-var array = new int[fileArray.Length, 2];
+var arrays = new List<int[]>();
+arrays.Add(new int[fileArray.Length]);
+arrays.Add(new int[fileArray.Length]);
+
 var sortedDiff = new int[fileArray.Length];
 
 for (var i = 0; i < fileArray.Length; i++)
 {
     var splitLine = fileArray[i].Split("   ");
-    array[i, 0] = int.Parse(splitLine[0]);
-    array[i, 1] = int.Parse(splitLine[1]);
+    arrays[0][i] = int.Parse(splitLine[0]);
+    arrays[1][i] = int.Parse(splitLine[1]);
 }
 
-Array.Sort(array);
+Array.Sort(arrays[0]);
+Array.Sort(arrays[1]);
 
 for (var i = 0; i < fileArray.Length; i++)
 {
-    sortedDiff[i] = Math.Abs(array[i, 0] - array[i, 1]);
+    sortedDiff[i] = Math.Abs(arrays[0][i] - arrays[1][i]);
 }
 
 // var cumulativeDistance = sortedDiff.Sum();
